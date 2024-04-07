4a6a4d4fd3c9eab0f5609516754bfc9c86fc860b
Utkarshalways <utkarshshrma08@gmail.com>
2024-04-07 17:54:36 +0530

    Header Working Fully

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,20 +12,54 @@ const Header = () => {
           alt=""
           className="h-8"
         />
-        <p className=" font-bold">Logo</p>
+        <p className=" font-bold text-l sm:text-sm">Chronicle Connect</p>
       </div>
 
-      <div
-        className="HAMBURGER-ICON space-y-2 " 
-        onClick={() => setnavOpen((prev) => !prev)} // toggle isNavOpen state on click
-      >
-      {console.log(navOpen)}
-        <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-        <span className="block h-0.5 w-8 animate-bounce bg-white"></span>
-        <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-      </div>
+      <section>
+        <div
+          className="HAMBURGER-ICON space-y-2 hidden  sm:inline "
+          onClick={() => setnavOpen((prev) => !prev)} // toggle isNavOpen state on click
+        >
+          {/* {console.log(navOpen)} */}
+          <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+          <span className="block h-0.5 w-8 animate-bounce bg-white"></span>
+          <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+        </div>
 
-      <div className={!navOpen?"hidden":""}>
+        <div className={navOpen ? "showMenuNav" : "hideMenuNav"}>
+          {" "}
+          
+          <div
+            className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+            onClick={() => setnavOpen(false)} // change isNavOpen state to false to close the menu
+          >
+            <svg
+              className="h-8 w-8 text-gray-600"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
+          </div>
+          <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px] bg-purple-100 text-black">
+            <li className="border-b border-gray-400 my-8 uppercase">
+              <a href="/about">About</a>
+            </li>
+            <li className="border-b border-gray-400 my-8 uppercase">
+              <a href="/portfolio">Portfolio</a>
+            </li>
+            <li className="border-b border-gray-400 my-8 uppercase">
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <div className={navOpen ? "hidden" : " sm:hidden"}>
         <ul
           className="flex gap-2 items-center 
        "
@@ -36,6 +70,25 @@ const Header = () => {
           <li className=" m-2 cursor-pointer hover:underline">Style</li>
         </ul>
       </div>
+      <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
     </header>
   );
 }
